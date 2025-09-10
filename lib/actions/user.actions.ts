1408c8ecfad14837bb6483226fde15a3279ca97e
Dan Imran <dan.imran97@gmail.com>
2025-09-10 00:43:06 -0400

    helper functions and stuff

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,7 +2,7 @@
 
 import { createAdminClient } from "@/lib/appwrite";
 import { appwriteConfig} from "@/lib/appwrite/config";
-import { Query } from "node-appwrite"
+import { Query, ID } from "node-appwrite"
 
 const getUserByEmail = async (email: string) => {
     const { databases} = await createAdminClient();
@@ -10,8 +10,28 @@ const getUserByEmail = async (email: string) => {
     const result = await databases.listDocuments(
         appwriteConfig.databaseId,
         appwriteConfig.usersCollectionId,
-        queries: [Query.equal("email", [email])],
+        [Query.equal("email", [email])],
     )
+
+    return result.total > 0 ? result.documents[0] : null
+}
+
+const handleError = (error: unknown, message: string) => {
+    console.log(error, message)
+    throw error
+}
+
+const sendEmailOTP = async ({ email}: {email: string}) => {
+    const { account } = await createAdminClient();
+
+    try {
+        const session = await account.createEmailToken(ID.unique(), email)
+
+        console.log(session)
+        return session.userId
+    } catch (error) {
+        handleError(error, "Failed to send email OTP")
+    }
 }
 
 const createAccount = async ({ 
@@ -20,4 +40,26 @@ const createAccount = async ({
         email: string;
     }) => {
         const existingUser = await getUserByEmail(email)
+
+        const accountId = await sendEmailOTP({ email });
+        if(!accountId) throw new Error("Failed to send an OTP")
+
+        if(!existingUser) {
+            const {databases} = await createAdminClient()
+
+            await databases.createDocument(
+                appwriteConfig.databaseId,
+                appwriteConfig.usersCollectionId,
+                ID.unique(),
+                {
+                    fullName,
+                    email,
+                    avatar:
+                    "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541",
+                    accountId,
+                },
+            )
+        }
+
+        return parseStringify({ accountId })
 }
