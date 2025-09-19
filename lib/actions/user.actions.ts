8c6bf162108f2ef7bac1859585b59685791ba174
Dan Imran <dan.imran97@gmail.com>
2025-09-18 20:33:11 -0400

    missing semicolon fixed LOL

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -23,7 +23,7 @@ const handleError = (error: unknown, message: string) => {
     throw error
 }
 
-const sendEmailOTP = async ({ email}: {email: string}) => {
+export const sendEmailOTP = async ({ email}: {email: string}) => {
     const { account } = await createAdminClient();
 
     try {
@@ -75,7 +75,7 @@ export const verifySecret = async ({
     try {
         const { account} = await createAdminClient() 
 
-        const session = await account.createSession(accountId, password)
+        const session = await account.createSession(accountId, password);
 
         (await cookies()).set('appwrite-session', session.secret, {
             path: '/',
