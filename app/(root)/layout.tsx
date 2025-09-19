cf292f49bc06bd87dbffabd46bb304c3487925c3
Dan Imran <dan.imran97@gmail.com>
2025-09-19 17:46:01 -0400

    work on layout.tsx for (root)

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Layout = ({ children }: {children: React.ReactNode}) => {
+  return (
+
+    <main className="flex h-screen">
+        Sidebar
+        <section className="flex h-full flex-1">
+
+        </section>
+    </main>
+)
+}
+export default Layout;
