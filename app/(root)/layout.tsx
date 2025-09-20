07babf3fcd8f9082819468579fd55c36c569841c
Dan Imran <dan.imran97@gmail.com>
2025-09-20 15:24:45 -0400

    gotta fix invalid src prop error

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -12,7 +12,7 @@ const Layout = async ({ children }: {children: React.ReactNode}) => {
   
   return (
     <main className="flex h-screen">
-        <Sidebar />
+        <Sidebar {...currentUser}/>
         <section className="flex h-full flex-1 flex-col">
           <MobileNavigation />
           <Header />
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,11 +3,17 @@
 import React from "react";
 import Link from "next/link"
 import Image from "next/image";
-import { avatarPlaceholderUrl, navItems } from "@/constants";
+import { navItems } from "@/constants";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 
-const Sidebar = () => {
+interface Props {
+    fullName: string;
+    avatar: string;
+    email: string;
+}
+
+const Sidebar = ({ fullName, avatar, email}: Props) => {
     const pathname = usePathname()
 
   return <aside className="sidebar">
@@ -65,7 +71,7 @@ const Sidebar = () => {
 
             <div className="sidebar-user-info">
                 <Image 
-                    src={avatarPlaceholderUrl}
+                    src={avatar}
                     alt="Avatar"
                     width={44}
                     height={44}
