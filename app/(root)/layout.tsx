5e1ea13bcc8dbc7bfbb23e3e6d9e7f288fe6a723
Dan Imran <dan.imran97@gmail.com>
2025-09-20 14:53:46 -0400

    add components for main page and move page to root

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,22 @@
+import Header from "@/components/Header";
+import MobileNavigation from "@/components/MobileNavigation";
+import Sidebar from "@/components/Sidebar";
 import React from "react";
+import { getCurrentUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-const Layout = ({ children }: {children: React.ReactNode}) => {
+const Layout = async ({ children }: {children: React.ReactNode}) => {
+  const currentUser = await getCurrentUser();
+
+  if(!currentUser) return redirect("/sign-in")
+  
   return (
-
     <main className="flex h-screen">
-        Sidebar
-        <section className="flex h-full flex-1">
-
+        <Sidebar />
+        <section className="flex h-full flex-1 flex-col">
+          <MobileNavigation />
+          <Header />
+          <div className="main-content">{children}</div>
         </section>
     </main>
 )
